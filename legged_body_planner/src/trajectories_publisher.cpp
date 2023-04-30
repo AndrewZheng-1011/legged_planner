c499d620ed124d902d808c63272ff150c73f9e41
Andrew Zheng <85125684+AndrewZheng-1011@users.noreply.github.com>
2023-04-30 11:48:32 -0400

    Update trajectories_publisher.cpp
    Use plan horizon

--- legged_body_planner/src/trajectories_publisher.cpp
+++ legged_body_planner/src/trajectories_publisher.cpp
@@ -146,7 +146,7 @@ ocs2::TargetTrajectories TrajectoriesPublisher::cmdVelToTargeTrajectories(
       current_pose.tail(3);  // YPR of euler angles
   ocs2::vector_t cmd_vel_rot = ocs2::getRotationMatrixFromZyxEulerAngles(ypr) *
                                cmd_vel.head(3);  // cmd vel w.r.t. body frame
-  const ocs2::scalar_t time_to_target = planner_config_.TIME_TO_TARGET;
+  const ocs2::scalar_t time_to_target = planner_config_.PLAN_HORIZON;
 
   // Define target pose
   ocs2::vector_t target(6);
